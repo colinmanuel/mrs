b549b6829d0e70cbfa3fca438aec4c14e345c22f
Colin Manuel <colin.manuel.com>
2015-05-09 16:52:18 -0500

    optimize performance

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'js/scriptgrunt.js'],
       options: {
         globals: {
           jQuery: true
