420b9375c1c5f211c9669dd8fd045f5dd7e5af1a
Colin Manuel <colin.manuel.com>
2016-05-25 15:51:24 -0500

    update mrs

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
